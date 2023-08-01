099324e47c5b47b305e06c974ebbb697f4074b17
ali77557 <141191001+ali77557@users.noreply.github.com>
2023-08-02 02:58:10 +0330

    Update index.js
    update contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x6878554d77210f38eb428b1129acf74ff317d6f7";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
